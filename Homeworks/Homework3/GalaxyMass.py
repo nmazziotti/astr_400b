1bfd4a0beaea7b1a45a9a9e61a667d034a111989
Nicolas Mazziotti <nwmazziotti@gmail.com>
2025-02-06 12:42:53 -0700

    Finished HW3

--- Homeworks/Homework3/GalaxyMass.py
+++ Homeworks/Homework3/GalaxyMass.py
@@ -14,7 +14,7 @@ def ComponentMass(filename, ParticleType):
         M_tot (astropy units) is the total mass of the galaxy component in 1e12 Msun. 
     """
 
-    time, N_tot, data = Read(filename)
+    time, N_tot, data = Read(filename) # Retrieve time of SnapNumber, total number of particles in model, and particle data table
 
     # Filter data table to given part type (DM represented by 1.0, Disk is 2.0, and Bulge is 3.0)
     if ParticleType == 'DM':
@@ -23,7 +23,8 @@ def ComponentMass(filename, ParticleType):
         index = np.where(data['type'] == 2.0)
     elif ParticleType == 'Bulge':
         index = np.where(data['type'] == 3.0)
+    
+    # Sum over all particle masses in the array to get total mass for the component
+    M_tot = np.sum(data[index]['m']) / 100 # Data is already stored in units of 1e10 Msun, so divide by 100 to get 1e12 Msun
 
-    M_tot = np.sum(data[index]['m']) / 100
-
-    return np.round(M_tot, 3)
+    return np.round(M_tot, 3) # Round to 3 decimal places
